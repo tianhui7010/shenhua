ec53c4c0358b90e38c028b78de923ea7f44235d7
tianhui <526136300@qq.com>
2019-03-25 16:46:49 +0800

    增加启动注解

--- wukong/src/main/java/com/nb/wukong/WukongApplication.java
+++ wukong/src/main/java/com/nb/wukong/WukongApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+//启动类
 @SpringBootApplication
 @EnableScheduling
 public class WukongApplication {
