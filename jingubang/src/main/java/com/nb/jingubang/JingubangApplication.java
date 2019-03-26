fc955a3e7e0d5cb4154c5e0cdb062104214a5bb5
tianhui <526136300@qq.com>
2019-03-26 11:50:18 +0800

    增加F

--- jingubang/src/main/java/com/nb/jingubang/JingubangApplication.java
+++ jingubang/src/main/java/com/nb/jingubang/JingubangApplication.java
@@ -3,7 +3,7 @@ package com.nb.jingubang;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//启动类
+//启动类 F
 @SpringBootApplication
 public class JingubangApplication {
 
