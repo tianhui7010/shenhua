91140afc56936be5745cb1426a243a8804319fb4
tianhui <526136300@qq.com>
2019-04-11 14:19:57 +0800

    delete WukongApplication Class ZhuShi

--- wukong/src/main/java/com/nb/wukong/WukongApplication.java
+++ wukong/src/main/java/com/nb/wukong/WukongApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-//启动类 //
 @SpringBootApplication
 @EnableScheduling
 public class WukongApplication {
