946df3104a9c822afa31188568717646544c1171
tianhui <526136300@qq.com>
2019-04-11 14:16:13 +0800

    delete JinGuBangApplication Class ZhuShi

--- jingubang/src/main/java/com/nb/jingubang/JingubangApplication.java
+++ jingubang/src/main/java/com/nb/jingubang/JingubangApplication.java
@@ -3,7 +3,6 @@ package com.nb.jingubang;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//启动类 FFF
 @SpringBootApplication
 public class JingubangApplication {
 
